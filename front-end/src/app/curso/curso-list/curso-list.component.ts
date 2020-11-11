196a347abd9760f5f5c7af8fe2d6f38dc64cc144
matheussouza89 <matheus.souza89@fatec.sp.gov.br>
2020-11-11 14:07:21 +0000

    (11/11) Término do componente e início do componente de listagem

--- front-end/src/app/curso/curso-list/curso-list.component.ts
+++ front-end/src/app/curso/curso-list/curso-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CursoService } from '../curso.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-curso-list',
@@ -10,13 +11,37 @@ export class CursoListComponent implements OnInit {
 
     cursos: any = [] // Vetor vazio
 
-    displayedColumns : string[] = ['nome','carga_horaria','nivel','valor_curso']
+    displayedColumns : string[] = ['nome','carga_horaria','nivel','valor_curso','editar','excluir']
 
-  constructor(private cursoSrv : CursoService) { }
+  constructor(
+      private cursoSrv : CursoService,
+      private snackBar : MatSnackBar
+      ) { }
 
    async ngOnInit() {
       return this.cursos = await this.cursoSrv.listar()
       console.log(this.cursos)
+  }
+
+  async excluir(id : string){
+      if(confirm('Deseja realmente excluir este item?')){
+        try{
+            // 1) Efetuar a exclusão
+            await this.cursoSrv.excluir(id)
+            // 2) Atualizar os dados da tabela
+            this.ngOnInit()
+            // 3) Dar um feedback de sucesso para o usuário
+            this.snackBar.open('Item excluido com sucesso.','Entendi',{
+                duration: 5000 // 5 segundos
+            })
+        }
+        catch(erro){
+            // 4) Dar um feedback de erro para o usuário
+            this.snackBar.open('ERRO: não foi possivel excluir este item.','Que pena!',{
+                duration: 5000 // 5 segundos
+            })
+        }
+      }
   }
 
 }
