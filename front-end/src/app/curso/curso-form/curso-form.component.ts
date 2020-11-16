5bc9cb76a0669cfd896e144e7c1d50aaab6cff3d
matheussouza89 <matheus.souza89@fatec.sp.gov.br>
2020-11-16 14:09:52 +0000

    (16/11) Início do componente de formulário

--- front-end/src/app/curso/curso-form/curso-form.component.ts
+++ front-end/src/app/curso/curso-form/curso-form.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursoFormComponent implements OnInit {
 
+    title : string = 'Novo Curso'
+
+    curso : any = {} //Objeto vazio, nome da entidade no SINGULAR
+
+    niveis : any = [
+        {valor: 'Básico', descr: 'Básico'},
+        {valor: 'Intermediário', descr: 'Intermediário'},
+        {valor: 'Avançado', descr: 'Avançado'}
+    ]
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  salvar(form){
+
   }
 
 }
