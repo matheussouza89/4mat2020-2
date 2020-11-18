30d72345d79b760c7ed4d292a29dedd5927ff967
matheussouza89 <matheus.souza89@fatec.sp.gov.br>
2020-11-18 14:14:12 +0000

    (18/11) Término dos primeiros componentes de listagem + formulário

--- front-end/src/app/curso/curso.service.ts
+++ front-end/src/app/curso/curso.service.ts
@@ -20,4 +20,16 @@ export class CursoService {
         return this.http.request('DELETE',this.apiUri,{body: {_id: id}}).toPromise()
     }
 
+    novo(body : any){
+        return this.http.post(this.apiUri, body).toPromise()
+    }
+
+    obterUm(id: string){
+        return this.http.get(this.apiUri + '/' + id).toPromise()
+    }
+
+    atualizar(body : any){
+        return this.http.put(this.apiUri, body).toPromise()
+    }
+
 }
