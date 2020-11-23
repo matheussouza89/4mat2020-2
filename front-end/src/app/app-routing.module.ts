42707035efd6fa4365a4ae1310f090f8f87ab9ba
matheussouza89 <matheus.souza89@fatec.sp.gov.br>
2020-11-23 14:07:24 +0000

    (23/11) Segundo componente de listagem

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CursoListComponent } from './curso/curso-list/curso-list.component';
 import { CursoFormComponent } from './curso/curso-form/curso-form.component';
+import { TurmaListComponent } from './turma/turma-list/turma-list.component';
 
 const routes: Routes = [
     // Rotas no Angular NUNCA começam com barra
     {path: 'curso', component: CursoListComponent},
     {path: 'curso/novo', component: CursoFormComponent},
-    {path: 'curso/:id', component: CursoFormComponent}
+    {path: 'curso/:id', component: CursoFormComponent},
+
+    {path: 'turma', component: TurmaListComponent}
 ];
 
 @NgModule({
